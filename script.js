83670b5bf4c276c78e7e628b6382dac25c44e55e
RensWest <94118091+RensWest@users.noreply.github.com>
2021-12-02 18:34:26 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-var logo = document.querySelector('.container')
+var open = document.querySelector('.hamburger')
+var close = document.querySelector('.close')
 var menu = document.querySelector('.menu')
 
-logo.addEventListener('click', function(){
-    menu.classList.toggle('showmenu')
+open.addEventListener('click', function(){
+    menu.classList.add('showmenu')
+})
+
+close.addEventListener('click', function(){
+    menu.classList.remove('showmenu')
 })
